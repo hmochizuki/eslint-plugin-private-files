658e236ee40fc2bf9fa8710cbc69bf8dee26e6ff
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2025-01-24 23:34:11 +0900

    feat: 絶対パスにも対応

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { Rule } from 'eslint';
 import path from 'path';
 
+const privateFileExtension = '.private.ts';
+export const errorMessage = 'Imports from outside the same directory are not allowed for *.private.ts files.';
+
 const rule: Rule.RuleModule = {
   meta: {
     type: 'problem',
     docs: {
       description: 'Disallow imports from outside the same directory for *.private.ts files',
       category: 'Best Practices',
-      recommended: false
+      recommended: true
     },
     schema: [] // no options
   },
@@ -15,34 +18,15 @@ const rule: Rule.RuleModule = {
     return {
       ImportDeclaration(node) {
         const filePath = context.filename;
+        const dir = path.dirname(filePath);
         const importPath = node.source.value as string;
-
-        if (filePath.endsWith('.private.ts')) {
-          const dir = path.dirname(filePath);
-          const resolvedPath = path.resolve(dir, importPath);
-
-          if (path.isAbsolute(importPath)) {
-            console.log("absolute");
-            // 絶対パスの場合、同一フォルダかどうかをチェック
-            if (!resolvedPath.startsWith(dir)) {
-              context.report({
-                node,
-                message: 'Absolute imports must be from the same directory for *.private.ts files.'
-              });
-            }
-          } else {
-            console.log("relative");
-            console.log(importPath.startsWith('./'));
-            // 相対パスの場合、./で始まることをチェック
-            if (!importPath.startsWith('./')) {
-              console.log("relative error");
-              context.report({
-                node,
-                message: 'Relative imports must start with "./" for *.private.ts files.'
-              });
-            }
-          }
-        }
+        if(!filePath.endsWith(privateFileExtension)) return;
+        if(importPath.startsWith("./") || path.dirname(importPath) === dir) return;
+        context.report({
+          node,
+          message: errorMessage
+        });
+        return;
       }
     };
   }
@@ -50,6 +34,6 @@ const rule: Rule.RuleModule = {
 
 export default {
   rules: {
-    'no-external-imports': rule
+    'no-external-imports': rule,
   }
 };
