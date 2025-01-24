42ea37714940f86c945bd15fae839f6522f56bd2
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2025-01-24 23:30:42 +0900

    test: 相対パスの場合のテストも追加

--- __tests__/no-external-imports.test.ts
+++ __tests__/no-external-imports.test.ts
@@ -1,32 +1,68 @@
-import { describe, it, expect } from 'vitest';
-import { RuleTester } from 'eslint';
-import rule from '../src/index';
+import { describe, it } from "vitest";
+import { RuleTester } from "eslint";
+import rule, { errorMessage } from "../src/index";
 
 const ruleTester = new RuleTester();
 
-describe('no-external-imports', () => {
-  it('should pass for valid imports', () => {
-    ruleTester.run('no-external-imports', rule.rules['no-external-imports'], {
-      valid: [
-        {
-          code: "import './localModule';",
-          filename: 'src/example.private.ts',
-        },
-      ],
-      invalid: [],
+describe("no-external-imports", () => {
+  describe("relative imports", () => {
+    it("should pass for valid imports", () => {
+      ruleTester.run("no-external-imports", rule.rules["no-external-imports"], {
+        valid: [
+          {
+            code: "import './localModule';",
+            filename: "src/example.private.ts",
+          },
+        ],
+        invalid: [],
+      });
+    });
+    it("should fail for invalid relative imports", () => {
+      ruleTester.run("no-external-imports", rule.rules["no-external-imports"], {
+        valid: [],
+        invalid: [
+          {
+            code: "import '../externalModule';",
+            filename: "src/example.private.ts",
+            errors: [
+              {
+                message: errorMessage,
+              },
+            ],
+          },
+        ],
+      });
     });
   });
 
-  it('should fail for invalid imports', () => {
-    ruleTester.run('no-external-imports', rule.rules['no-external-imports'], {
-      valid: [],
-      invalid: [
-        {
-          code: "import '../externalModule';",
-          filename: 'src/example.private.ts',
-          errors: [{ message: 'Relative imports must start with "./" for *.private.ts files.' }],
-        },
-      ],
+  describe("absolute imports", () => {
+    it("should pass for valid absolute imports", () => {
+      ruleTester.run("no-external-imports", rule.rules["no-external-imports"], {
+        valid: [
+          {
+            code: "import 'src/module/internal.private.ts';",
+            filename: "src/module/example.private.ts",
+          },
+        ],
+        invalid: [],
+      });
+    });
+
+    it("should fail for invalid absolute imports", () => {
+      ruleTester.run("no-external-imports", rule.rules["no-external-imports"], {
+        valid: [],
+        invalid: [
+          {
+            code: "import 'src/absolute/path/to/module';",
+            filename: "src/example.private.ts",
+            errors: [
+              {
+                message: errorMessage,
+              },
+            ],
+          },
+        ],
+      });
     });
   });
-}); 
+});
